5b9a169a35679f063f7f24974a0726f8519884ff
Manya <manyab485@gmail.com>
2024-07-09 15:09:30 +0530

    create the files

---
+++ ott-website/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+         <h1>OTT Website</h1> 
+       </div>
+      
+
+  );
+}
+
+export default App;

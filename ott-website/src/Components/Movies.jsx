e13cce9e7561c2d7b5385e7d795aaa64700096f1
Manya <manyab485@gmail.com>
2024-08-05 15:44:25 +0530

    cards update

--- ott-website/src/Components/Movies.jsx
+++ ott-website/src/Components/Movies.jsx
@@ -1,13 +1,14 @@
 import React, { useRef, useState, useEffect } from "react";
 import "../index.css";
-import { IoArrowForward, IoArrowBack } from "react-icons/io5";
+import { IoArrowForward, IoArrowBack, IoTimeOutline, IoEyeOutline } from "react-icons/io5";
+import { AiFillStar } from "react-icons/ai"; 
 import Img2 from "../Images/Posters/Movies/Trending/Hanuman.jpg";
 import Img3 from "../Images/Posters/Movies/Must Watch/Oppenheimer.jpg";
 import Img4 from "../Images/Posters/Movies/Trending/Wakanda.jpeg";
 import Img16 from "../Images/Posters/Movies/Genres/Drama/Ruslan.jpg";
 
 // Reusable GenreSection component
-function GenreSection({ genreTitle, cards, isGrid }) {
+function GenreSection({ genreTitle, images, isGrid }) {
   const scrollRef = useRef(null);
   const [showArrows, setShowArrows] = useState({ left: false, right: true });
 
@@ -45,33 +46,110 @@ function GenreSection({ genreTitle, cards, isGrid }) {
   return (
     <div className="movie-sub-container">
       <h3>{genreTitle}</h3>
-      <div className="movie-slider-container">
+      <div className={`movie-slider-container ${genreTitle.toLowerCase()}`}>
         {showArrows.left && (
           <div className="arrow left-arrow" onClick={scrollLeft}>
             <IoArrowBack />
           </div>
         )}
         <div className="movie-cards" ref={scrollRef}>
-          {cards.map((card, index) => (
-            <div className={`genre-card ${isGrid ? "grid-card" : "single-image-card"}`} key={index}>
-              <div className="genre-content">
-                <h5>{card.title}</h5>
-                <IoArrowForward />
-              </div>
+          {images.map((image, index) => (
+            <div
+              className={`genre-card ${isGrid ? "grid-card" : "single-image-card"} ${
+                genreTitle === "Thriller" ? "thriller-card" : ""
+              } ${genreTitle === "Fantasy" ? "fantasy-card" : ""}`}
+              key={index}
+            >
               {isGrid ? (
                 <>
-                  <div className="grid-image"><img src={card.image} alt={`${card.title} ${index + 1}`} /></div>
-                  <div className="grid-image"><img src={card.image} alt={`${card.title} ${index + 1}`} /></div>
-                  <div className="grid-image"><img src={card.image} alt={`${card.title} ${index + 1}`} /></div>
-                  <div className="grid-image"><img src={card.image} alt={`${card.title} ${index + 1}`} /></div>
+                  <div className="grid-image">
+                    <img src={image.src} alt={image.title || `${genreTitle} ${index + 1}`} />
+                    {/* Title and Fixed Arrow for grid layout */}
+                    <div className="card-title-and-arrow">
+                      <h5 className="card-title">{image.title}</h5>
+                      <IoArrowForward className="fixed-arrow" />
+                    </div>
+                  </div>
+                  <div className="grid-image">
+                    <img src={image.src} alt={image.title || `${genreTitle} ${index + 1}`} />
+                    {/* Title and Fixed Arrow for grid layout */}
+                    <div className="card-title-and-arrow">
+                      <h5 className="card-title">{image.title}</h5>
+                      <IoArrowForward className="fixed-arrow" />
+                    </div>
+                  </div>
+                  <div className="grid-image">
+                    <img src={image.src} alt={image.title || `${genreTitle} ${index + 1}`} />
+                    {/* Title and Fixed Arrow for grid layout */}
+                    <div className="card-title-and-arrow">
+                      <h5 className="card-title">{image.title}</h5>
+                      <IoArrowForward className="fixed-arrow" />
+                    </div>
+                  </div>
+                  <div className="grid-image">
+                    <img src={image.src} alt={image.title || `${genreTitle} ${index + 1}`} />
+                    {/* Title and Fixed Arrow for grid layout */}
+                    <div className="card-title-and-arrow">
+                      <h5 className="card-title">{image.title}</h5>
+                      <IoArrowForward className="fixed-arrow" />
+                    </div>
+                  </div>
                 </>
               ) : (
-                <img src={card.image} alt={card.title} className="single-cover-image" />
+                <>
+                  <img
+                    src={image.src}
+                    alt={image.title || image.alt}
+                    className={`single-cover-image ${
+                      genreTitle === "Thriller" ? "thriller-image" : ""
+                    } ${genreTitle === "Fantasy" ? "fantasy-image" : ""}`}
+                  />
+
+                  {/* Title and Fixed Arrow for other genres */}
+                  {genreTitle !== "Thriller" && genreTitle !== "Fantasy" && (
+                    <div className="card-title-and-arrow">
+                      <h5 className="card-title">{image.title}</h5>
+                      <IoArrowForward className="fixed-arrow" />
+                    </div>
+                  )}
+
+                  {/* Thriller Cards */}
+                  {genreTitle === "Thriller" && (
+                    <div className="thriller-details">
+                      <div className="thriller-time">
+                        <IoTimeOutline className="thriller-icon" />
+                        <span>1h 30min</span>
+                      </div>
+                      <div className="thriller-views">
+                        <IoEyeOutline className="thriller-icon" />
+                        <span>2K</span>
+                      </div>
+                    </div>
+                  )}
+
+                  {/* Fantasy Cards */}
+                  {genreTitle === "Fantasy" && (
+                    <div className="fantasy-details">
+                      <div className="fantasy-time">
+                        <IoTimeOutline className="fantasy-icon" />
+                        <span>1h 57min</span>
+                      </div>
+                      <div className="fantasy-rating">
+                        <AiFillStar className="fantasy-star" />
+                        <AiFillStar className="fantasy-star" />
+                        <AiFillStar className="fantasy-star" />
+                        <AiFillStar className="fantasy-star" />
+                        <AiFillStar className="fantasy-star half" />
+                        <span>20K</span>
+                      </div>
+                    </div>
+                  )}
+                </>
               )}
             </div>
           ))}
         </div>
-        {showArrows.right && (
+        {showArrows.right && genreTitle !== "Thriller" && genreTitle !== "Fantasy" && (
           <div className="arrow right-arrow" onClick={scrollRight}>
             <IoArrowForward />
           </div>
@@ -85,82 +163,99 @@ function GenreSection({ genreTitle, cards, isGrid }) {
 export default function Movies() {
   const genreSections = [
     {
-      genreTitle: "Action Movies",
+      genreTitle: "Action",
       isGrid: true,
-      cards: [
-        { title: "Hanuman", image: Img16 },
-        { title: "Oppenheimer", image: Img2 },
-        { title: "Wakanda", image: Img3 },
-        { title: "Ruslan", image: Img4 },
+      images: [
+        { src: Img16, title: "Action Movie 1" },
+        { src: Img2, title: "Action Movie 2" },
+        { src: Img3, title: "Action Movie 3" },
+        { src: Img4, title: "Action Movie 4" },
+        { src: Img4, title: "Action Movie 5" },
+        { src: Img4, title: "Action Movie 6" },
+        { src: Img4, title: "Action Movie 7" },
+        { src: Img4, title: "Action Movie 8" },
       ],
     },
     {
-      genreTitle: "Adventure Movies",
-      isGrid: false,
-      cards: [
-        { title: "Adventure 1", image: Img16 },
-        { title: "Adventure 2", image: Img2 },
-        { title: "Adventure 3", image: Img3 },
-        { title: "Adventure 4", image: Img4 },
+      genreTitle: "Adventure",
+      isGrid: true,
+      images: [
+        { src: Img16, title: "Adventure Movie 1" },
+        { src: Img2, title: "Adventure Movie 2" },
+        { src: Img3, title: "Adventure Movie 3" },
+        { src: Img4, title: "Adventure Movie 4" },
+        { src: Img3, title: "Adventure Movie 5" },
+        { src: Img3, title: "Adventure Movie 6" },
+        { src: Img3, title: "Adventure Movie 7" },
+        { src: Img3, title: "Adventure Movie 8" },
+        { src: Img3, title: "Adventure Movie 9" },
       ],
     },
     {
-      genreTitle: "Comedy Movies",
+      genreTitle: "Comedy",
       isGrid: false,
-      cards: [
-        { title: "Comedy 1", image: Img16 },
+      images: [
+        { src: Img16, title: "Comedy Movie 1" },
+        { src: Img16, title: "Comedy Movie 2" },
+        { src: Img16, title: "Comedy Movie 3" },
+        { src: Img16, title: "Comedy Movie 4" },
+        { src: Img16, title: "Comedy Movie 5" },
+        { src: Img16, title: "Comedy Movie 6" },
+        { src: Img16, title: "Comedy Movie 7" },
       ],
     },
     {
-      genreTitle: "Drama Movies",
+      genreTitle: "Drama",
       isGrid: false,
-      cards: [
-        { title: "Drama 1", image: Img16 },
+      images: [
+        { src: Img2, title: "Drama Movie 1" },
+        { src: Img4, title: "Drama Movie 2" },
+        { src: Img3, title: "Drama Movie 3" },
+        { src: Img4, title: "Drama Movie 4" },
+        { src: Img4, title: "Drama Movie 5" },
+        { src: Img4, title: "Drama Movie 6" },
+        { src: Img4, title: "Drama Movie 7" },
+        { src: Img4, title: "Drama Movie 8" },
       ],
     },
     {
-      genreTitle: "Fantasy Movies",
+      genreTitle: "Fantasy",
       isGrid: false,
-      cards: [
-        { title: "Fantasy 1", image: Img16 },
-        { title: "Fantasy 2", image: Img16 },
-        { title: "Fantasy 1", image: Img16 },
-        { title: "Fantasy 1", image: Img16 },
-        { title: "Fantasy 1", image: Img16 },
-        { title: "Fantasy 1", image: Img16 },
-        { title: "Fantasy 1", image: Img16 },
-        { title: "Fantasy 1", image: Img16 },
+      images: [
+        { src: Img16, title: "Fantasy Movie 1" },
+        { src: Img2, title: "Fantasy Movie 2" },
+        { src: Img4, title: "Fantasy Movie 3" },
+        { src: Img4, title: "Fantasy Movie 4" },
+        { src: Img3, title: "Fantasy Movie 5" },
+        { src: Img4, title: "Fantasy Movie 6" },
+        { src: Img16, title: "Fantasy Movie 7" },
+        { src: Img16, title: "Fantasy Movie 8" },
       ],
     },
     {
-      genreTitle: "Thriller Movies",
+      genreTitle: "Thriller",
       isGrid: false,
-      cards: [
-        { title: "Thriller 1", image: Img16 },
-        { title: "Thriller 2", image: Img16 },
-        { title: "Thriller 3", image: Img16 },
-        { title: "Thriller 4", image: Img16 },
-        { title: "Thriller 5", image: Img16 },
-        { title: "Thriller 6", image: Img16 },
-        { title: "Thriller 7", image: Img16 },
-        { title: "Thriller 8", image: Img16 },
-        { title: "Thriller 9", image: Img16 },
+      images: [
+        { src: Img16, title: "Thriller Movie 1" },
+        { src: Img16, title: "Thriller Movie 2" },
+        { src: Img16, title: "Thriller Movie 3" },
+        { src: Img16, title: "Thriller Movie 4" },
+        { src: Img16, title: "Thriller Movie 5" },
+        { src: Img16, title: "Thriller Movie 6" },
       ],
     },
   ];
 
   return (
-    <div className="movies-container">
-      <div className="movies-content">
-        {genreSections.map((section, index) => (
-          <GenreSection
-            key={index}
-            genreTitle={section.genreTitle}
-            cards={section.cards}
-            isGrid={section.isGrid}
-          />
-        ))}
-      </div>
+    <div className="main-movies-container">
+      {genreSections.map((section, index) => (
+        <GenreSection
+          key={index}
+          genreTitle={section.genreTitle}
+          images={section.images}
+          isGrid={section.isGrid}
+        />
+      ))}
     </div>
   );
 }

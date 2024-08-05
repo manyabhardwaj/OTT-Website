5b05b6f1f2739421193099013cb48984ac7274df
Manya <manyab485@gmail.com>
2024-08-05 21:45:01 +0530

    Changes

--- ott-website/src/Components/navbar.jsx
+++ ott-website/src/Components/navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "../index.css";
 import Logo from "../Images/Logo.png";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
 
 export default function Navbar() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <>
       <div className="navbar">
@@ -27,7 +35,35 @@ export default function Navbar() {
           </div>
         </div>
         <button className="primary-btn">Sign In</button>
+        <div className="hamburger-menu" onClick={toggleSidebar}>
+        <GiHamburgerMenu size={30} className='hamburgerIcon '/>
+        </div>
       </div>
+      {isSidebarOpen && (
+        <div className="sidebar">
+          <div className="sidebar-close" onClick={toggleSidebar}>
+          <IoClose  className='CloseIcon'/>
+          </div>
+          <div className="sidebar-menu">
+            <div className="nav-item-active">
+              <p className="p-large">Home</p>
+            </div>
+
+            <div className="nav-item">
+              <p className="p-large text-gray">Movies & Shows</p>
+            </div>
+
+            <div className="nav-item">
+              <p className="p-large text-gray">Subscriptions</p>
+            </div>
+
+            <div className="nav-item">
+              <p className="p-large text-gray">Support</p>
+            </div>
+            <button className="primary-btn">Sign In</button>
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
